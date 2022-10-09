7747219f63ae5edece64d690d3c3a8212da1f027
Maksim Botalov <dev.botalov@gmail.com>
2022-10-10 04:07:14 +0500

    Initial Commit

---
+++ BalancerSwiftUI/BalancerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalancerSwiftUIApp.swift
+//  BalancerSwiftUI
+//
+//  Created by Максим Боталов on 10.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct BalancerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
